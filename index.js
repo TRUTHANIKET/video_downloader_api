cce53b495ec13a4ebfa4edde0fdfcc1cb9405331
TRUTHANIKET <aniketsingh78698@gmail.com>
2023-06-07 12:03:10 +0530

    semi netlify push

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express=require("express")
 const ytdl = require("ytdl-core")
-
+const cors=require("cors")
 
 const app=express()
+app.use(cors());
 app.use(express.json())
 
-app.get("/",(req,res)=>{res.send("i am working")})
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.json("send the post request to /video endpoint and include url param in the body of post");
+})
 
 
 app.post("/video",async(req,res)=>{
-const url=await req.body.url
+try{
+    const url=await req.body.url
 const videoid=await ytdl.getURLVideoID(url)
 const meta=await ytdl.getBasicInfo(url)
 const store=meta.formats
@@ -20,9 +25,12 @@ const quality=await store.map(r=>{
 const videourl=await store.map(r=>{
     return(`${r.url} and ${r.qualityLabel} `)
 })
-console.log(videourl)
+console.log("request worked")
 res.status(200).json(store)
+}catch(e){
+    console.log(e.message)
+    res.status(500).json([{qualityLabel:"error check your url",url:"somethings wrong",mimeType:":)"}])
+}
 })
 
-
-app.listen(3000,()=>{console.log(`server running on http://localhost:3000`)})
+app.listen(process.env.PORT || 5000)
